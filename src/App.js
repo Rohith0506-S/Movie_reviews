cd330ae867bc2fc0fd335c6a9960a8483d783c32
Rohith0506-S <rohithsubr24@gmail.com>
2025-07-03 10:37:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieFeedbackForm from './components/MovieFeedbackForm';
+import MovieReview from './components/MovieReview'; // <-- Add this line
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Link,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Movie Feedback Forms</h1>
+
+        <div className='tabs'>
+          <Link to="/MovieFeedbackForm" className="tab-link">Feedback</Link>
+          <Link to="/MovieReview" className="tab-link">Review</Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Navigate to="/MovieFeedbackForm" />} />
+          <Route path="/MovieFeedbackForm" element={<MovieFeedbackForm />} />
+          <Route path="/MovieReview" element={<MovieReview />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
